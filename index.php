7b1ab93546eaadd78e1890ce79b9b77de4b1255e
Sodai-Komichi <83051052+Aso2101122@users.noreply.github.com>
2022-01-11 15:31:15 +0900

    完了ボタン、カテゴリ遷移

--- index.php
+++ index.php
@@ -31,8 +31,25 @@ $place_result = $sql->fetchAll(PDO::FETCH_ASSOC);
 
 
 
+//完了ボタンが押されたら
+if(isset($_POST['complete'])){
+    $sql = $stms->prepare('update shopping_list set complete_flag = 1 where id=?');
+    $sql->bindValue(1,$_POST['complete']);
+    $sql->execute();
+    $stmt = null;
+    header('Location: ./');
+    exit;
+}
 
-
+//完了取り消しボタンが押されたら
+if(isset($_POST['uncomplete'])){
+    $sql = $stms->prepare('update shopping_list set complete_flag = 0 where id=?');
+    $sql->bindValue(1,$_POST['uncomplete']);
+    $sql->execute();
+    $stmt = null;
+    header('Location: ./');
+    exit;
+}
 
 //削除ボタンが押されたら
 if(isset($_POST['delete'])){
@@ -56,25 +73,27 @@ if(isset($_POST['delete'])){
     <span>品名</span>
     <input type="text" name="name"/>
     <span>カテゴリー：</span>
-    <select name="category">
+    <select name="category" id="category-select" onChange="categoryAdd()">
         <?php foreach($category_result as $row):?>
         <option value="<?= $row['category_id']?>"><?= $row['category_name'] ?></option>
         <?php endforeach; ?>
+        <option value="category-add.php">カテゴリを追加</option>
     </select>
     <span>場所：</span>
-    <select name="place">
+    <select name="place" id="place-select" onChange="placeAdd()">
         <?php foreach($place_result as $row):?>
         <option value="<?= $row['place_id']?>"><?= $row['place_name'] ?></option>
         <?php endforeach; ?>
+        <option value="place-add.php">場所を追加</option>
     </select>
     <button type="submit" name="add">追加</button>
 </form>
 <?php
+echo '<h2>未完了</h2>';
 foreach($result as $row):?>
 <div class="item-row">
     <form action="index.php" method="post">
         <button type="submit" name="complete" value="<?= $row['id'] ?>"><img src="./img/checkbox_0.png" width="15px"/></button>
-        <input type="checkbox" name="complete" value="<?= $row['id'] ?>">
         <span><?= $row['name']?></span>
         <span><?= $row['category_id']?></span>
         <span><?= $row['place_id']?></span>
@@ -82,6 +101,7 @@ foreach($result as $row):?>
     </form>
 </div>
 <?php endforeach; ?>
+<h2>完了</h2>
 <div class="complete-row">
     <?php foreach($complete_result as $row):?>
     <div class="item-row">
@@ -95,5 +115,6 @@ foreach($result as $row):?>
     </div>
     <?php endforeach; ?>
 </div>
+<script src="script.js"></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-Document.getElementByid('');
+let category_select = document.getElementById("category-select");
+let place_select = document.getElementById("place-select");
+function categoryAdd(){
+    if(isNaN(category_select.value)){
+        window.location.href = category_select.value;
+    }
+}
+function placeAdd(){
+    if(isNaN(place_select.value)){
+        window.location.href = place_select.value;
+    }
+}
